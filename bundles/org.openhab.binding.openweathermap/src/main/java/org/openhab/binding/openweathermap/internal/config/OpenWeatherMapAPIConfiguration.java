0b58bc3473f5defe4915bc3a0470bdb64514d2b3
pelindberg <67074284+pelindberg@users.noreply.github.com>
2021-01-05 09:30:55 +0100

    Update OpenWeatherMapAPIConfiguration.java

--- bundles/org.openhab.binding.openweathermap/src/main/java/org/openhab/binding/openweathermap/internal/config/OpenWeatherMapAPIConfiguration.java
+++ bundles/org.openhab.binding.openweathermap/src/main/java/org/openhab/binding/openweathermap/internal/config/OpenWeatherMapAPIConfiguration.java
@@ -31,7 +31,7 @@ import org.openhab.binding.openweathermap.internal.handler.OpenWeatherMapAPIHand
 public class OpenWeatherMapAPIConfiguration {
     // supported languages (see https://openweathermap.org/current#multi)
     public static final Set<String> SUPPORTED_LANGUAGES = Collections.unmodifiableSet(Stream.of("ar", "bg", "ca", "cz",
-            "de", "el", "en", "es", "fa", "fi", "fr", "gl", "hr", "hu", "it", "ja", "kr", "la", "lt", "mk", "nl", "pl",
+            "da", "de", "el", "en", "es", "fa", "fi", "fr", "gl", "hr", "hu", "it", "ja", "kr", "la", "lt", "mk", "nl", "pl",
             "pt", "ro", "ru", "se", "sk", "sl", "tr", "ua", "vi", "zh_cn", "zh_tw").collect(Collectors.toSet()));
 
     public @Nullable String apikey;
